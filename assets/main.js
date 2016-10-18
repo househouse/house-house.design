e74424ba569b688c1ba2597072a303acc9e52b2b
Sean Durfee <durfee.sean@gmail.com>
2016-10-18 08:32:23 -0700

    Look at that gradient

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,12 @@
 //= require jquery3
 //= require jquery_ujs
 
-jQuery(document).ready(function($) {
-  $('.househouse').on('click', function(){
-    $(this).addClass('shrink-ray');
-    $('.copy').addClass('fadein');
-    return false;
-  });  
+$(function() {
+  $('.home').addClass('load-in together');
 });
+
+var loadIn = $('.home').removeClass('load-in');
+
+setTimeout(function(){
+  loadIn.addClass('shrink');
+}, 5000);
