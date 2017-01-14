9d74007a62aaadbe5dad83de963689a3cdb6a989
Ben G <ben@bengroulx.com>
2017-01-14 15:03:26 -0800

    Initial style pass

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-//= require jquery3
-
-$(function() {
-    $('body').removeClass('no-js');
-    $('.c-logo').addClass('c--animate-fade-in c--animate-together');
-    $('.c-hero').delay(3000).addClass('c--animate-shrink');
-});
