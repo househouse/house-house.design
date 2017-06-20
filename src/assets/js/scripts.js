293d738dda738cb7a02ed8a95020662ec333b94d
Sean Durfee <durfee.sean@gmail.com>
2017-06-20 17:28:53 +0100

    clean up modal;

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -52,47 +52,13 @@ class Modal {
     */
 
   init() {
-    // Create a node with a random ID to hold markup for each instance
-
-    const modalNodeInstance = document.createElement('div');
-    modalNodeInstance.id = `c-modal-instance-${Math.floor(Math.random() * 10000)}`;
-
-    // Create the container elements
-    this.modalElement = document.createElement('div');
-    this.modalElement.classList.add('c-modal', 'u-stack-modal');
-
-    // Clone the modal content into the inner modal container
-    const modalContents = this.content.cloneNode(true);
-    this.modalElement.appendChild(modalContents);
-
-
-    // Add the node to the DOM
-    document.body.appendChild(modalNodeInstance);
-
-    // Add inner container as a child element to the modal element
-    this.modalElement.appendChild(modalContents);
-
-    // Add modal element to the instance container
-    modalNodeInstance.appendChild(this.modalElement);
-
-    // Get the click region element (what triggers the modal)
-    // let trigger;
-    // if (this.trigger === null) {
-    //   trigger = this.self;
-    // } else {
-    //   trigger = this.trigger;
-    // }
-
-    // Add event listeners for modal
     for (const trigger of this.triggers) {
       trigger.addEventListener('click', (e) => {
         if (this.isOpen) {
           e.preventDefault();
-          console.log('>>>>>> T O G G L E    M O D A L   C L O S E <<<<<<');
           this.close();
         } else {
           e.preventDefault();
-          console.log('>>>>>> T O G G L E    M O D A L   O P E N <<<<<<');
           this.open();
         }
       });
@@ -145,136 +111,6 @@ const statementSelector = document.querySelector('.c-modal[data-modal="statement
 const statementContent = document.querySelector('[data-modal-inner="statement"]');
 const statementTrigger = document.querySelectorAll('[data-modal-trigger="statement"]');
 const statement = new Modal(statementSelector, statementContent, statementTrigger);
-
-// Old Modal
-// (function(e) {
-//   'use strict';
-//
-//   function i() {}
-//
-//   var t = function() {
-//     var e = {
-//       transition: 'transitionend',
-//     };
-//
-//     var i = document.createElement('b');
-//     var t;
-//     for (t in e) {
-//       if (i.style[t] !== null) {
-//         return e[t];
-//       }
-//     }
-//   }();
-//
-//   function n(e, i) {
-//     var t = document.querySelectorAll(e);
-//     var n = -1;
-//     var o = t.length;
-//     while (++n < o) {
-//       t[n].addEventListener('click', i);
-//     }
-//   }
-//
-//   function o(e, l) {
-//     var a = this;
-//
-//     l = l || {};
-//
-//     if (!(a instanceof o)) {
-//       return new o(e, l);
-//     }
-//     a.elem = e;
-//     a.onShow = l.onShow || i;
-//     a.onShowEnd = l.onShowEnd || i;
-//     a.onHide = l.onHide || i;
-//     var s = l.onHideEnd || i;
-//
-//     a.onHideEnd = function() {
-//       e.style.display = 'none';
-//       s(e);
-//     };
-//
-//     var r = a.fade = t ? l.fade : false;
-//
-//     a.scrollTop = l.scrollTop !== null ? l.scrollTop : true;
-//     a.isVisible = false;
-//
-//     var c = e.querySelector(l.dialogSelector);
-//
-//     n(l.showSelector || '.js-modal-show', function(e) { a.show(e.target); });
-//     n(l.hideSelector || '.js-modal-hide', function(e) { a.hide(e.target); });
-//
-//     e.addEventListener('click', function(i) {
-//       i.target === e && a.hide(e);
-//     });
-//
-//     if (r) {
-//       var u = (r.duration || '0.25s') + ' ' + (r.timingFunction || 'ease') + ' opacity';
-//       e.style.cssText += 'transition:' + u + ';';
-//       e.addEventListener(t, function() {
-//           (a.isVisible ? a.onShowEnd : a.onHideEnd)(e);
-//       });
-//     }
-//   }
-//
-//   o.prototype.show = function(e) {
-//     var i = this;
-//     var t = i.elem;
-//     var n = t.style;
-//     if (!i.isVisible) {
-//       document.body.style.overflow = 'hidden';
-//       if (i.fade) {
-//         n.opacity = 0;
-//       }
-//       n.display = 'block';
-//       if (i.scrollTop) {
-//         t.scrollTop = 0;
-//       }
-//       if (i.fade) {
-//         setTimeout(function() {
-//           n.opacity = 1;
-//         }, 0)
-//       }
-//       i.isVisible = true;
-//       i.onShow(t, e);
-//       !i.fade && i.onShowEnd(t);
-//     }
-//   };
-//
-//   o.prototype.hide = function (e) {
-//     var i = this;
-//     var t = i.elem;
-//     if (i.isVisible) {
-//       document.body.style.overflow = '';
-//       if (i.fade) {
-//         t.style.opacity = 0;
-//       }
-//       i.isVisible = false;
-//       i.onHide(t, e);
-//       !i.fade && i.onHideEnd(t);
-//     }
-//   };
-//
-//   if (typeof module === 'object') {
-//     module.exports = o;
-//   } else {
-//     e.modal = o;
-//   }
-// })(this);
-//
-// var elem = document.querySelector('.c-modal');
-//
-// var opts = {
-//   showSelector: '.js-modal-show',
-//   hideSelector: '.js-modal-hide',
-//   dialogSelector: '.c-modal__content',
-//   fade: {
-//     duration: '0.25s',
-//     timingFunction: 'linear'
-//   }
-// };
-//
-// modal(elem, opts);
 
 
 // Google Analytics
