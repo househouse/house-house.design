777eaba37f7b94166a1808a81c9da18ce8187da3
Sean Durfee <durfee.sean@gmail.com>
2017-08-15 12:59:04 +0100

    update router paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-import Home from '@/components/vHome/v-home'
-import NotFound from '@/components/vNotFound/v-not-found'
-import Thanks from '@/components/vThanks/v-thanks'
+import Home from '@/components/vHome/vHome';
+import NotFound from '@/components/vNotFound/vNotFound';
+import Thanks from '@/components/vThanks/vThanks';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -24,5 +24,5 @@ export default new Router({
       name: 'Thanks',
       component: Thanks,
     },
-  ]
-})
+  ],
+});
