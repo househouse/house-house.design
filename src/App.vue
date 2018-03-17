a6e73d5037db6e8fb11ae695db3a516edcc42fa3
Ben Groulx <ben@bengroulx.com>
2018-03-17 15:45:31 -0700

    Adding content to pages

--- src/App.vue
+++ src/App.vue
@@ -26,38 +26,75 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0;
-}
+    :root {
+        --black: #101030;
+        --white: #f3f6fa;
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+        --grey-dark: #54657f;
+        --grey-med: #b0bbcb;
+        --grey-light: #e6e9ee;
 
-main {
-  text-align: center;
-  margin-top: 40px;
-}
+        --indigo-dark: #4745d1;
+        --indigo-med: #3c69d9;
+        --indigo-light: #37a1fc;
 
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
+        --violet-dark: #8b3384;
+        --violet-med: #9857ac;
+        --violet-light: #b083c8;
 
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+        --orange-dark: #f44631;
+        --orange-med: #fc6932;
+        --orange-light: #fd9a59;
+
+        --green-dark: #419f77;
+        --green-med: #60b584;
+        --green-light: #72c87c;
+
+        --font--primary: 'UntitledSansTest-Regular', -apple-system, BlinkMacSystemFont, 'Roboto', 'Helvetica Neue', sans-serif;
+        --font--secondary: 'TiemposHeadline-Bold', 'Georgia', serif;
+        --font--code: 'Menlo', monospace;
+    }
+    .no-js .a-fade-in {
+        opacity: 0;
+    }
+    body {
+        background-color: var(--white);
+        color: var(--black);
+    }
+    .c-contain {
+        max-width: 30em;
+    }
+    .c-section.--small {
+        padding: 5% 10%;
+    }
+    .c-section:not(.--small) {
+        padding: 10%;
+    }
+    .c-shapes {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: -1;
+        overflow: hidden;
+        width: 100%;
+        height: 100%;
+    }
+    .c-shapes__container {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    @media screen and (min-width:600px) {
+        .c-shapes__container {
+            top: -35%;
+            left: 35%;
+            transform: rotate(45deg) scale(2);
+        }
+    }
+    .c-shapes__single {
+        fill: currentColor;
+        transition: all ease-out 2s 1s;
+    }
 </style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,60 @@
 <template lang="pug">
-    div.t-contact
-        h1 Contact
+    section.c-section
+        div.c-contain
+            h1.c-title We should work together
+
+            div.c-text-content(style="color:var(--grey-dark);")
+
+                div.c-text-content.u-measure-medium.u-margin-bottom-lg--small-only.u-margin-bottom-xl--small-up
+
+                    p.u-flexbox.u-align-middle
+                        span.u-block.u-circle.u-margin-right-sm(style='background:var(--green-med); width:0.5rem; height:0.5rem;')
+                        span.u-block.u-uppercase.u-text-size-small.u-tracked-loose.u-color-dark Available starting Q2 2018
+
+                    p We’re often head-down, jamming out creating something really cool. But we’ll always make time to hear about your challenges, or if you’re just wanting to say a quick hello. So give us a shout, and let’s make some magic happen.
+
+                    ul
+                        li.u-inline-block.u-margin-right-md
+                            a(href='https://househouse-hq.slack.com/', target='_blank', rel='noopener') Slack
+                        li.u-inline-block.u-margin-right-md
+                            a(href='https://github.com/househouse/', target='_blank', rel='noopener') GitHub
+                        li.u-inline-block
+                            a(href='https://twitter.com/house-house/', target='_blank', rel='noopener') Twitter
+            div.u-margin-bottom-lg--small-only.u-margin-bottom-xl--small-up.u-padding-left-md--small-up.u-padding-right-md--small-up#contact-us
+
+                    div.u-context.u-bg-light-grey.u-corners.u-padding-top-lg.u-padding-top-xxl--medium-up.u-padding-bottom-lg.u-padding-bottom-xxl--medium-up.u-padding-left-md.u-padding-right-md
+
+                        form.u-block.u-measure-medium#contact-form(action='https://formspree.io/ben@house-house.design', method='POST')
+
+
+                            div.u-flexbox--small-up.u-clip
+                                div.u-context.u-border-left-dark.u-border-bottom-dark.u-width-full--small-only.u-width-1of2--small-up.u-float-left.u-margin-right-md--small-up.u-margin-bottom-md(style='height:3rem;')
+                                    label.u-position.u-top-halfway.u-realign-center-vert.u-text-size-small.u-color-primary.u-padding-left-md.u-stack-basic(for='form-name')
+                                        | Your Name
+                                    input.u-position.u-top-0.u-left-0.u-right-0.u-bottom-0.u-width-full.u-border-0.u-text-size-base.u-padding-left-md#form-name(required='', type='text', name='_name')
+                                div.u-context.u-border-left-dark.u-border-bottom-dark.u-width-full--small-only.u-width-1of2--small-up.u-float-left.u-margin-bottom-md(style='height:3rem;')
+                                    label.u-position.u-top-halfway.u-realign-center-vert.u-text-size-small.u-color-primary.u-padding-left-md.u-stack-basic(for='form-email')
+                                        | Your Email
+                                    input.u-position.u-top-0.u-left-0.u-right-0.u-bottom-0.u-width-full.u-border-0.u-text-size-base.u-padding-left-md#form-email(required='', type='text', name='_replyto')
+
+
+                            div.u-flexbox--small-up.u-clip
+                                div.u-context.u-border-left-dark.u-border-bottom-dark.u-width-full.u-float-left.u-margin-bottom-md(style='height:12rem;')
+                                    label.u-position.u-bottom-0.u-text-size-small.u-color-primary.u-padding-left-md.u-padding-bottom-md.u-stack-basic(for='form-message' style='pointer-events:none;')
+                                        | Your Message
+                                    textarea.u-position.u-top-0.u-left-0.u-right-0.u-bottom-0.u-width-full.u-border-0.u-text-size-base.u-padding-left-md.u-padding-top-md.u-padding-bottom-md.u-padding-right-md#form-message(required='', placeholder='e.g. Hello, House House! I have a problem with ________ that I need some guidance on… I think I may need a designer! My budget is $__,____ and I would like a solution in ____ weeks. Can you help?', name='_message')
+
+
+                            div.u-flexbox--small-up.u-clip
+                                button.u-bg-primary.u-border-0.u-width-full--small-only.u-width-1of2--small-up.u-width-1of4--medium-up.u-color-light.u-padding-top-md.u-padding-bottom-md.u-float-right.u-text-size-base#form-submit(name='submit', type='submit')
+                                    | Send
+
+                            div(hidden='')
+                                input(type='text', name='_gotcha')
+                                input(type='hidden', name='_next', value='/thanks')
+                                input(type='hidden', name='_subject', value='Hi there, House House!')
+                                input(type='hidden', name='_cc', value='sean@house-house.design')
+
 </template>
 
 <script>
