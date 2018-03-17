87982bd97ae12f7f2441b954f7205e64eeaa44d8
Sean <sean@house-house.design>
2018-03-17 15:55:51 -0700

    load scss

--- src/App.vue
+++ src/App.vue
@@ -25,39 +25,7 @@ export default {
 };
 </script>
 
-<style>
-body {
-  margin: 0;
-}
+<style lang='scss'>
+@import 'src/assets/styles/main.scss';
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
