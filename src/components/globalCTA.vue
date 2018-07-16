ea14649a2acd35e204ee2a77af077dc3809ade9e
Ben Groulx <ben@bengroulx.com>
2018-07-16 16:17:33 -0700

    Global Nav + Footer styling tweaks

--- src/components/globalCTA.vue
+++ src/components/globalCTA.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
-    aside.u-bg-black.u-color-white.u-padding-top-xlarge.u-padding-bottom-large#globalCTA
+    aside.u-bg-black.u-color-white.u-padding-bottom-medium.u-padding-bottom-xlarge--medium-up.u-padding-top-medium.u-padding-top-xlarge--medium-up#globalCTA
         div.c-contain.c-text-content.u-text-align-center--medium-up.u-max-width-2of3--medium-up
             p.u-text-family-secondary.u-text-size-medium.u-baseline-medium
+
                 | We’re helping innovative teams like yours build apps, create
                 | styleguides, scale systems, and reach their goals. Let’s get a
                 | conversation going and see if our solution-focused, scalable
