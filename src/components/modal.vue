0cb7f2b34219a6d76ef954ebcec66302ee0e44c3
Ben Groulx <ben@bengroulx.com>
2018-12-10 13:42:03 -0800

    Modal component styling adjustments

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,99 +1,50 @@
 <template lang="pug">
-  transition(name="modal-slide")
-      article.modal.u-stack-modal.u-context(role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription")
-
-          header.modal-header(id="modalTitle")
-              slot(name="header")
-                | This is the default tile!
-              button.btn-close(type="button" @click="close")
-                | ×
-
-          section.modal-body.c-text-content(id="modalDescription")
-              slot(name="body")
-                | I'm the default body!
-
-          footer.modal-footer
-              slot(name="footer")
-                | I'm the default footer!
+transition(name="modal-slide")
+    article.c-modal.u-stack-modal.u-flexbox.u-flex-column(role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription")
+        header.c-modal__header.u-flexbox.u-align-center.u-justify-between(id="modalTitle")
+            h2.u-padding-top-xsmall
+                slot(name="header") Default Headline
+            button.c-modal__close.u-flexbox.u-text-size-large(type="button" @click="close") ×
+        section.c-modal__body.c-text-content(id="modalDescription" v-scroll-lock="open")
+            slot(name="body") Default body text; replace me!
+        footer.c-modal__footer
+            slot(name="footer") Default footer text; replace me!
 </template>
 
-<script>
-  export default {
-    name: 'modal',
 
-    methods: {
-      close() {
-        this.$emit('close');
-      },
+<script>
+export default {
+  name: 'modal',
+  data() {
+    return {
+      open: false,
+    };
+  },
+  methods: {
+    close() {
+      this.$emit('close');
     },
-  };
+  },
+};
 </script>
 
-<style lang="scss">
-  .modal {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    display: flex;
-    flex-direction: column;
-    width: 33vw;
-    overflow-x: auto;
-    background: var(--white);
-    box-shadow: 0 0 5em rgba(0, 0, 0, 0.15);
-  }
 
-  .modal-header,
-  .modal-footer,
-  .modal-body {
-    padding: 2vw 3vw;
-  }
+<style lang="scss" scoped>
+.modal-slide-enter,
+.modal-slide-leave-to {
+  opacity: 0.5;
+  box-shadow: 0 0 0 rgba(0, 0, 0, 0);
+  transform: translate(100%, 0);
+}
 
-  .modal-header,
-  .modal-footer {
-    display: flex;
-    align-items: center;
-  }
+.modal-slide-enter-to,
+.modal-slide-leave {
+  opacity: 1;
+  transform: translate(0, 0);
+}
 
-  .modal-header {
-    justify-content: space-between;
-    background-color: var(--grey-light);
-  }
-
-  .modal-footer {
-    border-top: 1px solid #eeeeee;
-    justify-content: flex-end;
-  }
-
-  .modal-body {
-    flex: 1;
-    overflow-x: hidden;
-    overflow-y: scroll;
-  }
-
-  .btn-close {
-    border: none;
-    font-size: 20px;
-    padding: 0;
-    cursor: pointer;
-    font-weight: bold;
-    background: transparent;
-  }
-
-  .modal-slide-enter,
-  .modal-slide-leave-to {
-    opacity: 0.5;
-    transform: translate(100%, 0);
-  }
-
-  .modal-slide-enter-to,
-  .modal-slide-leave {
-    opacity: 1;
-    transform: translate(0, 0);
-  }
-
-  .modal-slide-enter-active,
-  .modal-slide-leave-active {
-    transition: 200ms ease-in-out;
-  }
+.modal-slide-enter-active,
+.modal-slide-leave-active {
+  transition: 250ms cubic-bezier(1, 0, 0, 1);
+}
 </style>
