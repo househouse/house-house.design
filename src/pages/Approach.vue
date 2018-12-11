0dac8efc85ce30aea49bf93baacd677d4e09d085
Ben Groulx <ben@bengroulx.com>
2018-12-11 13:02:11 -0800

    Add SVG images to each modal

--- src/pages/Approach.vue
+++ src/pages/Approach.vue
@@ -88,6 +88,8 @@ div#pageApproach
     modal(v-show="isModalInterfaceVisible" @close="closeModalInterface" class="c--right c--blue-dark")
         div.u-color-white(slot="header") 🌀 Interface Design
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--interface.svg" style="max-height:15rem;")
             p.u-bold Intuitive digital experiences, created with intention and based on your brand story.
             p User interface design works when no one notices it's there. Getting to that place requires an iterative, design-centric approach that uses techniques blending psychology with technology. The end-user (your beloved audience) does what you want them to do (consensually, of course) without an awareness of the system itself.
             p That invisibility and intuitiveness requires a team who takes the time to learn exactly what your brand story is, what your audience needs and then has the experience to deliver.
@@ -98,6 +100,8 @@ div#pageApproach
     modal(v-show="isModalBrandVisible" @close="closeModalBrand" class="c--right c--blue-dark")
         div.u-color-white(slot="header") 🌄 Brand Identity
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--brand.svg" style="max-height:15rem;")
             p.u-bold Knowing who you are and why someone needs you is at the essence of identity.
             p Brand identity done well answers those questions with intention and awareness. Housemates approach the challenge of discovering your brand identity by using design thinking and story to get to the heart of why you do what you do and how you ought to communicate that to your lovely audience.
             p Specifically, we can help you refine a logo, colour palette, typeface, and full styleguide that will communicate who you are to your audience.
@@ -107,6 +111,8 @@ div#pageApproach
     modal(v-show="isModalDirectionVisible" @close="closeModalDirection" class="c--right c--blue-dark")
         div.u-color-white(slot="header") 👉 Digital Art Direction
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--direction.svg" style="max-height:15rem;")
             p.u-bold Like a symphony conductor, an art director unifies the vision of a design production.
             p Housemates bring clarity and definition to your project, creating the overall design by setting the tone and cadence of your workflow.
             p A workflow that can iterate and change as needed begins by answering the what, when, and where of your project. And these answers begin when our Housemates collaborate with your existing team to uncover your vision and help you execute.
@@ -118,6 +124,8 @@ div#pageApproach
     modal(v-show="isModalStrategyVisible" @close="closeModalStrategy" class="c--right c--blue-light")
         div.u-color-white(slot="header") ⚡️ Experience Strategy
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--strategy.svg" style="max-height:15rem;")
             p.u-bold Applying design-thinking and solution-focused methods to your customers' experience of your product means that you care.
             p And the result is ecstatic customers raving about your product. You may have seen it happen in the wild but how do you get that same level of joy in your own customers?
             p When you understand that your customer is more important than your product, you'll invest in CX — customer experience. And then you'll understand the importance of creating a strategy for their experience of your product.
@@ -131,6 +139,8 @@ div#pageApproach
     modal(v-show="isModalResearchVisible" @close="closeModalResearch" class="c--right c--blue-light")
         div.u-color-white(slot="header") 🔍 UX Research
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--research.svg" style="max-height:15rem;")
             p.u-bold Studying your user — using an interdisciplinary approach that combines design-thinking with anthropology, behavioural and social psychology — to find out what they require when interacting with your product.
             p The end result is happy customers using your product with delight. The path to getting to that place is called UX (user experience) research.
             p Housemates seek to understand your vision for a perfect experience of your product. We walk with you through your product journey, asking questions and guiding you as we listen and learn about what your user needs.
@@ -141,6 +151,8 @@ div#pageApproach
     modal(v-show="isModalServiceVisible" @close="closeModalService" class="c--right c--blue-light")
         div.u-color-white(slot="header") 🗺 Service Design
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--servicedesign.svg" style="max-height:15rem;")
             p.u-bold Apply product-design methodology to your service and you gain clarity to what is by nature a complicated system.
             p Designing a service requires the same fundamentals of design-theory as when designing products but with added input from human behavioural studies such as ethnography and anthropology. The subtle difference between the two lies in the fact that human interaction is a complex system which requires another layer of understanding and knowledge for the service to deliver expected results.
             p Because systems change as users interact with them, the design needs flexibility and a tolerance of uncertainty. This fundamental difference is often overlooked by brands—perhaps because we interact with services every day we interact with them in a mindless way. We fail to realize that to offer an excellent service, we need to add a mindful awareness of neuroscience and behavioural theory.
@@ -149,8 +161,10 @@ div#pageApproach
         router-link.c-button-block.c--small.u-bg-orange-dark.u-color-white(to='/contact' slot="footer") Start a design project &rarr;
 
     modal(v-show="isModalFEDVisible" @close="closeModalFED" class="c--right c--purple-light")
-        div.u-color-white(slot="header") ⌨️ Front-End Development
+        div.u-color-white(slot="header") ⌨️ Front-End Dev
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--FED.svg" style="max-height:15rem;")
             p.u-bold Turning design into an application or website (or any digital entity) using HTML, CSS, and JavaScript.
             p The front-end of a website takes the combined logic and problem-solving skills of a designer and combines them with programming knowledge to create apps and websites that are lovely to look at while also being performant for your users on any machine.
             p Housemates approach front end development by applying design principles to their code. We navigate between drawing with pencil and paper or digitally and the programming languages that best serve the product we're building. That means we're always learning, adding the next best way to accomplish what the product needs while remaining rooted in design-thinking.
@@ -160,6 +174,8 @@ div#pageApproach
     modal(v-show="isModalPWAVisible" @close="closeModalPWA" class="c--right c--purple-light")
         div.u-color-white(slot="header") 📱 Progressive Web Apps
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--PWA.svg" style="max-height:15rem;")
             p.u-bold Creating apps for all users, no matter their connectivity or machine quality — and in a browser. With a progressive web app, a user can access the app anywhere, on any device and even off-line.
             p Progressive web apps combine the rich experiences and high performance of native apps with the real-time updates, search-engine visibility and responsivity of a browser based web app. They can be a perfect solution for brands who have considered creating a native app but need the benefits of a web-based product.
             p As in everything we do, Housemates approach progressive web apps with a solution-focus. How does this PWA solve a problem four our client? Where does the PWA fit into the spectrum of the client's story arc? What is the best vision for this PWA, given all we have learned about our client and their needs?
@@ -169,6 +185,8 @@ div#pageApproach
     modal(v-show="isModalRWDVisible" @close="closeModalRWD" class="c--right c--purple-light")
         div.u-color-white(slot="header") 💻 Responsive Websites
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--RWD.svg" style="max-height:15rem;")
             p.u-bold Web sites that adapt to where your end-user discovers them — from the largest television screens to the smallest devices. Responsivity is foundational for all our web site design and development. In tandem with our design-based focus and systems-thinking, all sites we create for our clients look great wherever they are found.
             p Because Housemates approach all our work from a solution focus, responsive web site design and development is where we begin and end.
             p Our websites express our brand identity and as such, they need to flexibly fit to any device an end-user might find them on. This ethos means Housemates never stop learning web design and development—always staying at the edge of the best in web site design and development.
@@ -177,6 +195,8 @@ div#pageApproach
     modal(v-show="isModalTeambuildingVisible" @close="closeModalTeambuilding" class="c--right c--green-light")
         div.u-color-white(slot="header") 🤝 Team Building
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--teambuilding.svg" style="max-height:15rem;")
             p.u-bold For any group tasked with collaborating to create a product or service (or several), working together as a team can always benefit from added support with trained facilitators.
             p Investing in building and refining your team so that you end up with a more efficient, more productive system means that you all work together—whatever that means for your team.
             p Housemates approach team building from a design-ethos: systems-based, rational and solution-focused. We learn who you are, what you need to be at your best, and then guide you through exercises that produce happy, authentically-aware and creative teammates who approach their work together with foundational mindfulness.
@@ -187,6 +207,8 @@ div#pageApproach
     modal(v-show="isModalWorkshopsVisible" @close="closeModalWorkshops" class="c--right c--green-light")
         div.u-color-white(slot="header") 🎨 Workshops
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--workshops.svg" style="max-height:15rem;")
             p.u-bold House House believes that intentional design translates across platforms: digital, irl, wherever.
             p Learning how to work with intention, in a way that is solutions-focused and well-designed, equals enhanced performance, enhanced efficiency, and enhanced collaboration.
             p Changemaking ensues.
@@ -197,9 +219,11 @@ div#pageApproach
     modal(v-show="isModalTrainingVisible" @close="closeModalTraining" class="c--right c--green-light")
         div.u-color-white(slot="header") 📝 Training
         div.c-text-content(slot="body")
+            div.u-text-align-center
+                img(src="../assets/img/services--training.svg" style="max-height:15rem;")
             p.u-bold Sometimes, a learning boost is just what your team needs to get to the next level and produce award-winning and profit-creating products. Or whatever your brand does during working hours.
             p House House facilitators will come to your site and provide your team with what they need to improve efficiency, productivity and effectiveness in design, development, strategy or storytelling.
-            p By listening closely to your needs, Housemates can assemble a team who can deliver the knowledge your team needs quickly and {usually/always} painlessly. We'll discuss desired outcomes with you, create a working vision for what you want to happen, and then we'll deliver that to you. Should your needs change as we work together, we can iterate and pivot to deliver what you need.
+            p By listening closely to your needs, Housemates can assemble a team who can deliver the knowledge your team needs quickly and painlessly. We'll discuss desired outcomes with you, create a working vision for what you want to happen, and then we'll deliver that to you. Should your needs change as we work together, we can iterate and pivot to deliver what you need.
         router-link.c-button-block.c--small.u-bg-orange-dark.u-color-white(to='/contact' slot="footer") Start a training project &rarr;
 </template>
 
