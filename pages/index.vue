7049507b55ae9c2403ef63f5e49b23ab25c46f77
Ben Groulx <ben@bengroulx.com>
2019-07-05 09:54:43 -0700

    Copy editing/tweaking

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,50 @@
 <template>
   <div id="pageHome" class="u-bg-blue-dark">
+
     <section class="u-padding-bottom-medium u-padding-bottom-xlarge--medium-up u-color-white">
       <div class="c-contain u-context">
+
         <h1 class="c-heading-c--1 u-margin-bottom-small u-max-width-2of3--medium-up">
           <span class="u-visually-hidden">House House</span> A design studio and freelancer community.
         </h1>
-        <ul
-          class="u-max-width-1of2--medium-up u-text-list-bullets u-text-family-secondary u-text-size-medium u-baseline-base"
-        >
+
+        <ul class="u-max-width-1of2--medium-up u-text-list-bullets u-text-family-secondary u-text-size-medium u-baseline-medium">
+
           <li class="u-margin-top-small">
             Grow your team’s abilities, capacity, and potential by adding some extra Housemate hands to your next project
-            <span
-              class="u-no-wrap"
-            >
-              or sprint,
+            <span class="u-no-wrap">or sprint,
               <em class="u-color-grey-medium">or…</em>
             </span>
           </li>
+
           <li class="u-margin-top-small">
-            Hire our team of design and development consultants, strategists, and producers to turn your digital product ideas into
-            <span
-              class="u-no-wrap"
-            >a reality</span>.
+            Hire our team of design and development consultants and producers to turn your digital product ideas into
+            <span class="u-no-wrap">a reality</span>.
           </li>
+          
         </ul>
+
         <div class="u-position u-top-0 u-right-0 u-bg-cover u-bg-blancink-device x-device"></div>
-        <div
-          class="u-position u-bottom-0 u-right-0 x-device-description u-text-size-small u-baseline-small"
-        >
-          <p
-            class="u-margin-bottom-xsmall u-color-blue-light u-text-weight-bold u-no-break"
-          >◀ blanc ink</p>
-          <p
-            class="u-margin-bottom-xsmall u-color-grey-base"
-          >We helped the tattoo removal startup explore and build a bespoke web app.</p>
+
+        <div class="u-position u-bottom-0 u-right-0 x-device-description u-text-size-small u-baseline-small">
+          <p class="u-margin-bottom-xsmall u-color-blue-light u-text-weight-bold u-no-break">◀ blanc ink</p>
+          <p class="u-margin-bottom-xsmall u-color-grey-base">We helped the tattoo removal startup explore and build a bespoke web app.</p>
         </div>
+
       </div>
     </section>
+
     <section class="u-padding-bottom-medium u-padding-bottom-xlarge--medium-up u-padding-top-medium u-padding-top-xlarge--medium-up u-bg-grey-light u-color-grey-dark u-shadow-thick">
       <div class="c-contain">
-        <article
-          class="u-flexbox--medium-up u-align-end u-margin-bottom-medium u-margin-bottom-xlarge--medium-up"
-        >
-          <div
-            class="u-flex-shrink u-padding-right-medium--medium-up u-padding-right-xlarge--wide-up u-margin-bottom-base--medium-down"
-          >
+
+        <article class="u-flexbox--medium-up u-align-end u-margin-bottom-medium u-margin-bottom-xlarge--medium-up">
+          <div class="u-flex-shrink u-padding-right-medium--medium-up u-padding-right-xlarge--wide-up u-margin-bottom-base--medium-down">
             <IllustrationOne class="u-block" />
           </div>
           <div class="u-flex u-max-width-1of2--medium-up c-text-content">
             <h2 class="c-heading c--3 u-color-black">Flexible, so you don’t have to be</h2>
-            <p
-              class="u-text-family-secondary u-text-size-medium u-baseline-medium"
-            >Working with House House means having access to a community of talented design thinkers and doers — designers, developers, writers, strategists — ready to help you figure out your next project.</p>
+            <p class="u-text-family-secondary u-text-size-medium u-baseline-medium">
+              Working with House House means having access to a community of talented designers, developers, writers, strategists and producers.</p>
             <nuxt-link
               class="c-button-link c--purple c--with-icon u-text-secondary u-text-size-medium u-baseline-small"
               to="/housemates"
@@ -60,12 +53,9 @@
             </nuxt-link>
           </div>
         </article>
-        <article
-          class="u-flexbox--medium-up u-align-end u-margin-bottom-medium u-margin-bottom-xlarge--medium-up"
-        >
-          <div
-            class="u-flex-shrink u-order-2--medium-up u-padding-left-medium--medium-up u-padding-left-xlarge--wide-up u-margin-bottom-base--medium-down"
-          >
+
+        <article class="u-flexbox--medium-up u-align-end u-margin-bottom-medium u-margin-bottom-xlarge--medium-up">
+          <div class="u-flex-shrink u-order-2--medium-up u-padding-left-medium--medium-up u-padding-left-xlarge--wide-up u-margin-bottom-base--medium-down">
             <IllustrationThree class="u-block" />
           </div>
           <div class="u-flex u-max-width-1of2--medium-up c-text-content">
@@ -83,10 +73,9 @@
             </nuxt-link>
           </div>
         </article>
+
         <article class="u-flexbox--medium-up u-align-end">
-          <div
-            class="u-flex-shrink u-padding-right-medium--medium-up u-padding-right-xlarge--wide-up u-margin-bottom-base--medium-down"
-          >
+          <div class="u-flex-shrink u-padding-right-medium--medium-up u-padding-right-xlarge--wide-up u-margin-bottom-base--medium-down">
             <IllustrationTwo class="u-block" />
           </div>
           <div class="u-flex u-max-width-1of2--medium-up c-text-content">
@@ -103,8 +92,10 @@
             </nuxt-link>
           </div>
         </article>
+
       </div>
     </section>
+
   </div>
 </template>
 
